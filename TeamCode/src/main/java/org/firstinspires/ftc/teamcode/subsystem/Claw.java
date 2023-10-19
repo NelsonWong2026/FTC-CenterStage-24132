f7e3f9484acbdde7db7b8e0347e8c1fa30ef8df3
NelsonWong2026 <1361400308@k12.hi.us>
2023-10-18 17:00:12 -1000

    Added Claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Claw.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -7,22 +8,22 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.Constants;
 
 public class Claw {
-    private Servo claw;
+    private Servo Claw;
 
     public Claw() {
 
     }
 
     public void init(HardwareMap hwMap) {
-        this.claw = hwMap.get(Servo.class, Constants.Claw.Claw);
+        this.Claw = hwMap.get(Servo.class, Constants.Claw.Claw);
     }
 
-    public void setControl(Gamepad gamepad) {
+    public void setControl (Gamepad gamepad) {
         if (gamepad.a) {
-            claw.setPosition(1);
+            Claw.setPosition(1);
         }
         else if (gamepad.b) {
-            claw.setPosition(0);
+            Claw.setPosition(0);
         }
     }
 }
