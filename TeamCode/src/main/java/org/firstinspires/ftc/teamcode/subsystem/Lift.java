aee13157ae589d729fa3b1f0336c30b4cb26dd83
NelsonWong2026 <1361400308@k12.hi.us>
2023-10-18 17:00:55 -1000

    Added Lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Lift.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.Constants;
 
 public class Lift {
-    private DcMotor Lift;
+    private DcMotor Lift, Swing;
 
     public Lift() {
 
@@ -15,10 +15,13 @@ public class Lift {
 
     public void init(HardwareMap hwMap) {
         this.Lift = hwMap.get(DcMotor.class, Constants.Lift.Lift);
+        this.Swing = hwMap.get(DcMotor.class, Constants.Lift.Swing);
         this.Lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.Swing.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void setControl(Gamepad gamepad) {
+        Swing.setPower(-gamepad.right_stick_y);
         if (gamepad.dpad_up) {
             Lift.setPower(1);
         }
