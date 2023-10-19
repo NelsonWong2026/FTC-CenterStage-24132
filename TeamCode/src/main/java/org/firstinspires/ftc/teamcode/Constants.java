6f1be5a0c23d681828491713b48605ee813323a4
NelsonWong2026 <1361400308@k12.hi.us>
2023-10-18 17:00:43 -1000

    Added More Constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Constants {
+
+    public static final class MecanumDrive {
+        public static final String leftFront = "leftFront";
+        public static final String leftBack = "leftBack";
+        public static final String rightFront = "rightFront";
+        public static final String rightBack = "rightBack";
+    }
+
+    public static final class Time {
+        public static final int teleopTime = 120;
+        public static final int autoTime = 30;
+    }
+
+    public static final class Vision {
+        public static final String camera1 = "camera1";
+        public static final String camera2 = "camera2";
+    }
+
+    public static final class Claw {
+        public static final String Claw = "claw";
+    }
+
+    public static final class Lift {
+        public static final String Lift = "lift";
+        public static final String Swing = "swing";
+    }
+
+}
