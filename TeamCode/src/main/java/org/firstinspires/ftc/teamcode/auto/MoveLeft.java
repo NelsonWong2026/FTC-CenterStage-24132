95e916c64bf6231c8cbb19cb5d06081e34be234a
NelsonWong2026 <1361400308@k12.hi.us>
2023-11-01 16:14:24 -1000

    Added Simple Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MoveLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MoveLeft.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.subsystem.Claw;
 import org.firstinspires.ftc.teamcode.subsystem.MecanumDrive;
 
 
@@ -17,19 +18,23 @@ public class MoveLeft extends LinearOpMode{
 
     private MecanumDrive drive = new MecanumDrive();
     private ElapsedTime runtime = new ElapsedTime();
+    private Claw claw = new Claw();
 
     @Override
     public void runOpMode() {
         drive.init(hardwareMap);
+        claw.init(hardwareMap);
 
         waitForStart();
+        claw.setClawPos(1);
         drive.strafeLeft(0.7);
 
         runtime.reset();
-        while (opModeIsActive() && (runtime.milliseconds() < 5000)) {
+        while (opModeIsActive() && (runtime.milliseconds() < 4000)) {
 
         }
 
+        claw.setClawPos(0);
         drive.stop();
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MoveRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MoveRight.java
@@ -5,25 +5,30 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.subsystem.Claw;
 import org.firstinspires.ftc.teamcode.subsystem.MecanumDrive;
 
 @Autonomous(name = "MoveRight", group = "auto")
 public class MoveRight extends LinearOpMode {
     MecanumDrive drive = new MecanumDrive();
     ElapsedTime runtime = new ElapsedTime();
+    Claw claw = new Claw();
 
     @Override
     public void runOpMode() {
         drive.init(hardwareMap);
+        claw.init(hardwareMap);
 
         waitForStart();
+        claw.setClawPos(1);
         drive.strafeRight(0.7);
 
         runtime.reset();
-        while (opModeIsActive() && (runtime.milliseconds() < 5000)) {
+        while (opModeIsActive() && (runtime.milliseconds() < 4000)) {
 
         }
 
+        claw.setClawPos(0);
         drive.stop();
     }
 }
