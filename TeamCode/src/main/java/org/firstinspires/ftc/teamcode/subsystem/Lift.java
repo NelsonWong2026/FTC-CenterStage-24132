5d47ef6fa857eec696485c8b0d97abeb9ba358a8
NelsonWong2026 <1361400308@k12.hi.us>
2023-11-12 23:53:44 -1000

    Added lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Lift.java
@@ -7,36 +7,32 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.Constants;
 
 public class Lift {
-    private DcMotor Lift, Swing;
+    private DcMotor leftLift, rightLift;
 
     public Lift() {
 
     }
 
     public void init(HardwareMap hwMap) {
-        this.Lift = hwMap.get(DcMotor.class, Constants.Lift.Lift);
-        this.Swing = hwMap.get(DcMotor.class, Constants.Lift.Swing);
-        this.Lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        this.Swing.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.leftLift = hwMap.get(DcMotor.class, Constants.Lift.leftLift);
+        this.rightLift = hwMap.get(DcMotor.class, Constants.Lift.rightLift);
+        this.leftLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        this.rightLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void setControl(Gamepad gamepad) {
-        Swing.setPower(gamepad.right_stick_y);
-        if (gamepad.dpad_up) {
-            Lift.setPower(1);
+        if (gamepad.a) {
+            leftLift.setPower(1);
+            rightLift.setPower(1);
         }
-        else if (gamepad.dpad_down) {
-            Lift.setPower(-1);
+        else if (gamepad.b) {
+            leftLift.setPower(-1);
+            rightLift.setPower(-1);
         }
-        /*else if (gamepad.x) {
-            Swing.setPower(1);
-        }
-        else if (gamepad.y) {
-            Swing.setPower(-1);
-        }*/
         else {
-            Lift.setPower(0);
-            Swing.setPower(0);
+            leftLift.setPower(0);
+            rightLift.setPower(0);
         }
     }
+
 }
