a65595537a978dc729c11b81d7c1edf400cfabca
NelsonWong2026 <1361400308@k12.hi.us>
2023-11-12 23:52:59 -1000

    Added Arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Arm.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.subsystem;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Constants;
+
+public class Arm {
+    private DcMotor Arm;
+
+    public Arm() {
+
+    }
+
+    public void init(HardwareMap hwMap) {
+        this.Arm = hwMap.get(DcMotor.class, Constants.Arm.Arm);
+        this.Arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void setControl(Gamepad gamepad) {
+        if (gamepad.dpad_up) {
+            Arm.setPower(1);
+        }
+        else if (gamepad.dpad_down) {
+            Arm.setPower(-1);
+        }
+        else {
+            Arm.setPower(0);
+        }
+    }
+}
