bb56c53b4e95a6064a6c94fc4fcc402baa58c0d0
NelsonWong2026 <1361400308@k12.hi.us>
2023-11-12 23:53:13 -1000

    Added pivot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Claw.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -9,18 +10,23 @@ import org.firstinspires.ftc.teamcode.Constants;
 
 public class Claw {
     private Servo Claw;
+    private CRServo Pivot;
 
     public Claw() {
 
     }
 
+    public void init(HardwareMap hwMap) {
+        this.Claw = hwMap.get(Servo.class, Constants.Claw.Claw);
+        this.Pivot = hwMap.get(CRServo.class, Constants.Claw.Pivot);
+    }
+
     public void setClawPos(double Position) {
         Claw.setPosition(Position);
     }
-
-    public void init(HardwareMap hwMap) {
-        this.Claw = hwMap.get(Servo.class, Constants.Claw.Claw);
-    }
+    /*public void setPivotPos(double Position) {
+        Pivot.setPower(1);
+    }*/
 
     public void setControl (Gamepad gamepad) {
         if (gamepad.a) {
@@ -28,6 +34,15 @@ public class Claw {
         }
         else if (gamepad.b) {
             Claw.setPosition(0);
+        }
+        else if (gamepad.x) {
+            Pivot.setPower(1);
+        }
+        else if (gamepad.y) {
+            Pivot.setPower(-1);
+        }
+        else {
+            Pivot.setPower(0);
         }
     }
 }
