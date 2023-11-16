8f1f0caf4bf9f7b56ab27dc46fb59a1d2aa7998e
NelsonWong2026 <1361400308@k12.hi.us>
2023-11-15 15:27:17 -1000

    Nothing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/EasyOpenCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/EasyOpenCV.java
@@ -23,7 +23,7 @@ import org.openftc.easyopencv.OpenCvPipeline;
 import java.util.ArrayList;
 import java.util.List;
 
-//@TeleOp(name = "OpenCV Testing")
+@TeleOp(name = "OpenCV Testing")
 public class EasyOpenCV extends LinearOpMode {
 
     double cX = 0;
