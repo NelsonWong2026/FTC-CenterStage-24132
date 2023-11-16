6111209d096af05950760a22e8ca21ec0133d354
NelsonWong2026 <1361400308@k12.hi.us>
2023-11-15 21:22:45 -1000

    Added PID for arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/PIDF_Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/PIDF_Arm.java
@@ -1,7 +1,14 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.control.PIDFController;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Constants;
 
 public class PIDF_Arm extends OpMode {
     private PIDController controller;
@@ -11,15 +18,27 @@ public class PIDF_Arm extends OpMode {
 
     public static int target = 0;
 
-    private final double ticks_in_degrees;
+    private final double ticks_in_degree = 537.7 / 360;
+
+    private DcMotorEx arm_motor;
 
     @Override
     public void init() {
+        controller = new PIDController(p, i, d);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
+        arm_motor = hardwareMap.get(DcMotorEx.class, Constants.Arm.Arm);
     }
 
     @Override
     public void loop() {
+        controller.setPID(p, i, d);
+        int armPos = arm_motor.getCurrentPosition();
+        double pid = controller.calculate(armPos, target);
+        double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
 
+        double power = pid + ff;
+
+        arm_motor.setPower(power);
     }
 }
