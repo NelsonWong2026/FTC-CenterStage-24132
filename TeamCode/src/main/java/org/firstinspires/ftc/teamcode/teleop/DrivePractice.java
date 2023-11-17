4bd036b90578de2e1e4e7eaa32cd623558f119b6
NelsonWong2026 <1361400308@k12.hi.us>
2023-11-16 16:28:46 -1000

    Added DrivePractice

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DrivePractice.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystem.Claw;
+import org.firstinspires.ftc.teamcode.subsystem.Launcher;
+import org.firstinspires.ftc.teamcode.subsystem.Arm;
+import org.firstinspires.ftc.teamcode.subsystem.Lift;
+import org.firstinspires.ftc.teamcode.subsystem.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystem.PIDF_Arm;
+
+@TeleOp(name="DrivePractice", group="DrivePractice")
+public class DrivePractice extends OpMode {
+    private MecanumDrive drive = new MecanumDrive();
+
+    @Override
+    public void init() {
+        this.drive.init(hardwareMap);
+        telemetry.addData("Status","Initialized");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        this.drive.setControl(gamepad1);
+
+        telemetry.addData("Status","Enabled");
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        telemetry.addData("Status","Stopped");
+        telemetry.update();
+    }
+}
