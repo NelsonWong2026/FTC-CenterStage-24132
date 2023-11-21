1c1e3e13cc2d2af797e109b22c2158b0e2509e3d
NelsonWong2026 <1361400308@k12.hi.us>
2023-11-21 07:24:14 -1000

    Made robot controls field centric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/MecanumDrive.java
@@ -1,18 +1,18 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.Constants;
 
 public class MecanumDrive {
-    private DcMotor leftFront;
-    private DcMotor leftBack;
-    private DcMotor rightFront;
-    private DcMotor rightBack;
-
+    private DcMotor leftFront, leftBack, rightFront, rightBack;
+    private IMU imu;
     public MecanumDrive() {
 
     }
@@ -22,6 +22,13 @@ public class MecanumDrive {
         this.leftBack = hwMap.get(DcMotor.class, Constants.MecanumDrive.leftBack);
         this.rightFront = hwMap.get(DcMotor.class,Constants.MecanumDrive.rightFront);
         this.rightBack = hwMap.get(DcMotor.class,Constants.MecanumDrive.rightBack);
+        this.imu = hwMap.get(IMU.class, "imu");
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+
+        imu.initialize(parameters);
 
         this.leftFront.setDirection(DcMotor.Direction.REVERSE);
         this.leftBack.setDirection(DcMotor.Direction.REVERSE);
@@ -77,11 +84,28 @@ public class MecanumDrive {
         double x = gamepad.left_stick_x * 1.1;
         double rx = gamepad.right_stick_x;
 
+        if (gamepad.options) {
+            imu.resetYaw();
+        }
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        rotX *= 1.1;
+
+
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double frontLeftPower = (y + x + rx) / denominator;
+        /*double frontLeftPower = (y + x + rx) / denominator;
         double backLeftPower = (y - x + rx) / denominator;
         double frontRightPower = (y - x -rx) / denominator;
-        double backRightPower = (y + x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;*/
+
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX -rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
 
         leftFront.setPower(frontLeftPower);
         leftBack.setPower(backLeftPower);
